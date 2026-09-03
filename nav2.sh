81d1d762f3b9f6b833cc83bb2f16b9f4af5bc8e7
ekffo0204-git <ekffo0204@gmail.com>
2026-09-03 11:36:15 +0900

    feat: implement Nav2 navigation logic

---
+++ nav2.sh
@@ -0,0 +1,117 @@
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -2.100, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: 0.085, y: -0.905, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}}
+#   ]}"
+
+# #p1
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -2.1, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: 0.08, y: -2.400, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -2.1, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: 0.085, y: -0.905, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}}
+#   ]}"
+
+# #p2
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.420, y: -2.000, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.170, y: -2.000, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.170, y: -2.400, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.170, y: -2.000, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.420, y: -2.000, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: 0.085, y: -0.905, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}}
+#   ]}"
+
+
+# #p3
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.420, y: -2.000, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.420, y: -2.400, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.420, y: -2.000, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: 0.085, y: -0.905, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}}
+#   ]}"
+
+#n0
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.565, y: -0.705, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: 0.085, y: -0.905, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}}
+#   ]}"
+
+#f1
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -1.45, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: 0.030, y: -1.45, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -1.45, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: 0.085, y: -0.905, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}}
+#   ]}"
+
+#s2
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -1.69, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.2, y: -1.69, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -1.69, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.42, y: -2.40, z: 0.0}, orientation: {x: 0.0000000, y: 0.0000000, z: -0.7071068, w: 0.7071068}}}
+
+#   ]}"
+
+
+#dock -> normal
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.565, y: -0.705, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.565, y: -0.32, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.7071068, w: 0.7071068}}}
+#   ]}"
+
+
+# #dock -> p3
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -1.69, z: 0.0}, orientation: {x: 0.0000000, y: 0.0000000, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.43, y: -2.3, z: 0.0}, orientation: {x: 0.0000000, y: 0.0000000, z: -0.7071068, w: 0.7071068}}}
+#   ]}"
+
+# #dock -> f5
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -1.69, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.2, y: -1.69, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}}
+#   ]}"
+# #f5 -> p2
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -1.69, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.17, y: -1.69, z: 0.0}, orientation: {x: 0.0000000, y: 0.0000000, z: -0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.17, y: -2.400, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7071068, w: 0.7071068}}}
+#     ]}"
+
+#dock -> n5
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.450, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.565, y: -0.705, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.7071068, w: 0.7071068}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.570, y: -0.325, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.7071068, w: 0.7071068}}}
+#   ]}"
+
+
+# ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -1.69, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+#     {header: {frame_id: map}, pose: {position: {x: 0.085, y: -0.905, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}}
+
+#   ]}"
+
+ros2 action send_goal --feedback /follow_waypoints nav2_msgs/action/FollowWaypoints "{poses: [
+    {header: {frame_id: map}, pose: {position: {x: -0.440, y: -0.900, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}},
+    {header: {frame_id: map}, pose: {position: {x: 0.085, y: -0.905, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0000000, w: 1.0000000}}}
+    
+  ]}"
